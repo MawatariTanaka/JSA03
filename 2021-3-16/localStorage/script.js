8823b70397d6472153b85985544f52d8581ca734
MawatariTanaka <buinhatminh2005@gmail.com>
2021-03-18 21:08:05 +0700

    commit

--- 2021-3-16/localStorage/script.js
+++ 2021-3-16/localStorage/script.js
@@ -1,5 +1,16 @@
-localStorage.setItem("username", ["Bùi Nhật Minh", "Lương Minh Quân", "Dương Hồng Sơn"]);
-console.log(localStorage.username);
-localStorage.setItem("students", JSON.stringify(["Bùi Nhật Minh", "Trần Phương", "Duong Nguyen"]));
-console.log(JSON.parse(localStorage.students));
-console.log(JSON.parse(window.localStorage.getItem("students")));
+// 1
+localStorage.setItem("students", JSON.stringify(["An", "Anh", "Minh", "Phương", "Dương", "Khánh"]));
+let lesson = {
+    web: "html css js",
+    others: "open source",
+};
+localStorage.setItem("lessons", JSON.stringify(lesson));
+localStorage.setItem("numOfPeople", 6);
+// 2
+console.log(JSON.parse(localStorage.getItem("students")));
+console.log(JSON.parse(localStorage.getItem("lessons")));
+console.log(JSON.parse(localStorage.getItem("numOfPeople")));
+// 3
+localStorage.removeItem("lessons");
+// 4
+localStorage.clear();
